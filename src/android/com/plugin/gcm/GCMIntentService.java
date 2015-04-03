9c6e87326f0d23a1bdacd45a083acdb289329cd4
Sandy Mann <sandipmann@gmail.com>
2015-04-03 16:25:16 +1100

    catch JSONException

--- src/android/com/plugin/gcm/GCMIntentService.java
+++ src/android/com/plugin/gcm/GCMIntentService.java
@@ -76,11 +76,13 @@ public class GCMIntentService extends GCMBaseIntentService {
                     createNotification(context, extras);
                 }
                 else if (extras.getString("payload") != null) {
-                   JSONObject payload = new JSONObject(extras.getString("payload"));
-                   JSONObject data = payload.getJSONObject("data");
-                   String alert = data.getString("alert"); //parse puts the message as an alert if you don't use custom json payload
-                   extras.putString("message", alert);
-                   createNotification(context, extras);
+                    try {
+                       JSONObject payload = new JSONObject(extras.getString("payload"));
+                       JSONObject data = payload.getJSONObject("data");
+                       String alert = data.getString("alert"); //parse puts the message as an alert if you don't use custom json payload
+                       extras.putString("message", alert);
+                       createNotification(context, extras);
+                    } catch (JSONException e) {}
                 }
             }
         }
